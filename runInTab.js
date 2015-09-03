315fc802745a8a7c2ae2b7ce8685d608aa3cd5e6
Niklas Gollenstede <n.gollenstede@web.de>
2015-09-03 18:30:02 +0200

    allow options for runInTab

--- runInTab.js
+++ runInTab.js
@@ -1,17 +1,34 @@
 (function firefox(exports) { 'use strict';
 
-exports = (typeof module !== 'undefined') ? (() => {
+/**
+ * Loaded in the main process, provides a single function that allows to run functions in a tab process.
+ * @param  {Tab}           tab      The tab to run in
+ * @param  {...string}     modules  Modules to load before executing
+ * @param  {function}      script   The function to execute
+ * @param  {...any}        args     Arguments to the function
+ * @async
+ * @return {Promise(any)}  Promise to the value (or the value of the promise) returned by 'script'
+ */
+
+if (typeof module !== 'undefined') {
 	// main process
-	return (tab, modules, script) => new Promise((resolve, reject) => {
-		if (!script) {
-			script = modules; modules = [ ];
+	const __FILE__ = new Error().fileName.match(/ -> (resource:\/\/.*?)$/)[1];
+	const __REQUIRE__ = __FILE__.replace(/[^\/]*$/, 'require.js');
+
+	exports = (tab, ...args) => new Promise((resolve, reject) => {
+		const modules = [ __REQUIRE__, ];
+		let i = 0;
+		while (typeof args[i] !== 'function' && i < args.length) {
+			modules.push(args[i++]);
 		}
-		if (typeof script !== 'function') {
-			throw new TypeError('runInTab only accepts functions to execute');
-		}
+		modules.push(__FILE__);
+		const script = args[i];
+		if (!script) { throw new TypeError('Can\'t find \'script\' argument'); }
+		args.splice(0, i + 1);
+
 		const worker = tab.attach({
-			contentScriptFile: [ './../node_modules/es6lib/require.js', ].concat(modules, './../node_modules/es6lib/runInTab.js'),
-			contentScriptOptions: `return (${ script })()`,
+			contentScriptFile: modules,
+			contentScriptOptions: { script: `return (${ script })()`, args, },
 		});
 		worker.port.on("resolve", value => {
 			worker.destroy();
@@ -22,24 +39,25 @@ exports = (typeof module !== 'undefined') ? (() => {
 			reject(value);
 		});
 	});
-})() : (() => {
+} else {
 	// content script
 	const onError = error => {
 		// preserve Error objects through the JSON console.
 		if (error instanceof Error) {
-			error = [ 'columnNumber', 'fileName', 'lineNumber', 'message', 'stack', ]
+			error = [ 'columnNumber', 'fileName', 'lineNumber', 'message', 'name', 'stack', 'type', ]
 			.reduce((json, key) => ((json[key] = error[key]), json), { });
 		}
 		self.port.emit("reject", error);
 	};
 
 	try {
-		Promise.resolve(new String.constructor(self.options)()) // i.e. new Function(script), but 'script' was a function before and moves from more to less trusted code, so this is secure.
+		Promise.resolve(new String.constructor(self.options.script).apply(null, self.options.args))
+		// i.e. new Function(script), but 'script' was a function before and moves from more to less trusted code, so this is secure.
 		.then(value => self.port.emit("resolve", value))
 		.catch(onError);
 	} catch(error) {
 		onError(error);
 	}
-})();
+}
 
 const moduleName = 'es6lib/runInTab'; if (typeof module !== 'undefined') { module.exports = exports; } else if (typeof define === 'function') { define(moduleName, exports); } else if (typeof window !== 'undefined' && typeof module === 'undefined') { window[moduleName] = exports; } return exports; })({ });
